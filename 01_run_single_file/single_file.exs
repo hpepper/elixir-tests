163e54c9047962842d5cd47831928d13d9aa437a
Henry m <henry@m.example.net>
2021-10-26 13:55:43 +0200

    Add run single file

---
+++ 01_run_single_file/single_file.exs
@@ -0,0 +1,14 @@
+
+# define the module
+defmodule SingleFile do
+  def helloWorld do
+    IO.puts("Hellow Elixir world :-)")
+  end
+end
+
+# Call a function in the module
+SingleFile.helloWorld
+
+# To run this:
+#   iex single_files.exs
+# (press Ctrl-C twice to exit the 'eix' )
